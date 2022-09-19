72ad1ddbadefe2e5245906f82f414560859e697f
Vladimir Traihel <vovatraigel@gmail.com>
2022-09-19 19:54:50 +0300

    test commit

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './Main.module.scss'
 import {MainComponentType} from "../../app/state";
 import {SkillsInfo} from "./skillsInfo/SkillsInfo";
+import {Zoom} from "react-awesome-reveal";
+import Typewriter from 'typewriter-effect';
 
 type MainPropsType = {
     mainComponent: MainComponentType
@@ -14,16 +16,29 @@ export function Main(props: MainPropsType) {
             <div className={styles.mainContainer}>
                 <div className={styles.text}>
                     <h1>{props.mainComponent.h1}</h1>
-                    <SkillsInfo arrSkills={props.mainComponent.arrSkills}/>
+                    {/*<SkillsInfo arrSkills={props.mainComponent.arrSkills}/>*/}
+                    <Typewriter
+                        options={{
+                            strings: props.mainComponent.arrSkills,
+                            autoStart: true,
+                            loop: true,
+                        }}
+                    />
                     <p>{props.mainComponent.location}</p>
                     <a className={styles.myWorks} href={'#projects'}>{props.mainComponent.a1}</a>
                     <a className={styles.contactMe} href={'#contacts'}><span>{props.mainComponent.a2}</span> &#9660;</a>
                 </div>
+                <Zoom>
                 <div className={styles.photo}>
+
                     <div>
+
                         <img src={props.mainComponent.photo} alt={'photo'}/>
+
                     </div>
+
                 </div>
+</Zoom>
             </div>
         </div>
     );
--- src/components/main/skillsInfo/SkillsInfo.tsx
+++ src/components/main/skillsInfo/SkillsInfo.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import styles from './SkillsInfo.module.scss'
+import ReactTypingEffect from "react-typing-effect";
 
 type SkillsInfoPropsType = {
     arrSkills: string[]
@@ -7,20 +8,23 @@ type SkillsInfoPropsType = {
 
 export function SkillsInfo(props: SkillsInfoPropsType) {
 
-    const [skill, setSkill] = useState<string>(props.arrSkills[0])
-    const arrLength = props.arrSkills.length - 1
-    let i = 0
+    // const [skill, setSkill] = useState<string>(props.arrSkills[0])
+    // const arrLength = props.arrSkills.length - 1
+    // let i = 0
 
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            setSkill(props.arrSkills[i])
-            if (i < arrLength) i++
-            else i = 0
-        }, 1800)
-        return () => clearInterval(intervalID)
-    }, [])
+    // useEffect(() => {
+    //     const intervalID = setInterval(() => {
+    //         setSkill(props.arrSkills[i])
+    //         if (i < arrLength) i++
+    //         else i = 0
+    //     }, 1800)
+    //     return () => clearInterval(intervalID)
+    // }, [])
 
     return (
-        <h2 className={styles.skillsInfo}>{skill}</h2>
+        <h2 className={styles.skillsInfo}>
+            {/*{skill}*/}
+            <ReactTypingEffect text={props.arrSkills}/>
+        </h2>
     )
 }
