e274eca2ae5144cc6ab303091f8f29c75da1463a
Vladimir Traihel <vovatraigel@gmail.com>
2022-09-19 21:01:30 +0300

    Typewriter component

--- src/components/main/skillsInfo/SkillsInfo.tsx
+++ src/components/main/skillsInfo/SkillsInfo.tsx
@@ -1,26 +1,20 @@
-import React, {useEffect, useState} from 'react';
 import styles from './SkillsInfo.module.scss'
+import Typewriter from 'typewriter-effect';
 
 type SkillsInfoPropsType = {
     arrSkills: string[]
 }
 
 export function SkillsInfo(props: SkillsInfoPropsType) {
-
-    const [skill, setSkill] = useState<string>(props.arrSkills[0])
-    const arrLength = props.arrSkills.length - 1
-    let i = 0
-
-    useEffect(() => {
-        const intervalID = setInterval(() => {
-            setSkill(props.arrSkills[i])
-            if (i < arrLength) i++
-            else i = 0
-        }, 1800)
-        return () => clearInterval(intervalID)
-    }, [])
-
     return (
-        <h2 className={styles.skillsInfo}>{skill}</h2>
+        <h2 className={styles.skillsInfo}>
+            <Typewriter
+                options={{
+                    strings: props.arrSkills,
+                    autoStart: true,
+                    loop: true,
+                }}
+            />
+        </h2>
     )
 }
