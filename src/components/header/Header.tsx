1f242db3f7f232cccb8a82b47358feff00036d8f
Vladimir Traigel <vovatraigel@gmail.com>
2024-08-18 01:21:15 +0300

    test commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import {Nav} from './nav/Nav';
-import styles from './Header.module.scss'
-import {SvgSelector} from "../../common/svgSelector/SvgSelector";
-import {Links} from "./links/Links";
-import {Link} from "react-scroll";
-import {NavLink} from '../../types/enum/navLink';
+import { Link } from 'react-scroll';
+import { SvgSelector } from '../../common/svgSelector/SvgSelector';
+import { NavLink } from '../../types/enum/navLink';
+import styles from './Header.module.scss';
+import { Links } from './links/Links';
+import { Nav } from './nav/Nav';
 
 export function Header() {
+	// test git
 
-  return (
-      <header className={styles.header}>
-        <div className={styles.headerContainer}>
-          <Link
-              className={styles.logo}
-              to={NavLink.Home}
-              spy={true}
-              smooth={true}
-              offset={-60}
-              duration={500}
-          >
-            <SvgSelector svgName={"PortfolioSvg"}/>
-          </Link>
+	return (
+		<header className={styles.header}>
+			<div className={styles.headerContainer}>
+				<Link
+					className={styles.logo}
+					to={NavLink.Home}
+					spy={true}
+					smooth={true}
+					offset={-60}
+					duration={500}
+				>
+					<SvgSelector svgName={'PortfolioSvg'} />
+				</Link>
 
-          <Nav/>
+				<Nav />
 
-          <Links/>
-        </div>
-      </header>
-  );
+				<Links />
+			</div>
+		</header>
+	);
 }
